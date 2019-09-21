d8966d9f690aeafd9f11286b398dc42b2559a545
Julio_Cesar <julinho476@gmail.com>
2019-09-20 21:45:39 -0300

    Atividade05

--- Atividade05/src/app/app-routing.module.ts
+++ Atividade05/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'detalhes/:id', loadChildren: './detalhes/detalhes.module#DetalhesPageModule' },
-  { path: 'abas', loadChildren: './abas/abas.module#AbasPageModule' }
+  { path: 'detalhes/:id', loadChildren: './detalhes/detalhes.module#DetalhesPageModule', canActivate: [AuthGuard]},
+  { path: 'abas', loadChildren: './abas/abas.module#AbasPageModule', canActivate: [AuthGuard]}
 ];
 
 @NgModule({
--- Atividade05/src/app/login/login.page.ts
+++ Atividade05/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor( private authService: AuthService) { }
 
-  ngOnInit() {
+  ngOnInit() {this.authService.logout()
   }
 
 }
